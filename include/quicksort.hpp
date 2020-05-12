2e92afe73dc95500fbddf676fafe671b3064b2da
WinstThePhi <winstonhvo@gmail.com>
2020-05-11 20:15:48 -0700

    Final commit (hopefully); everything looks good

--- include/quicksort.hpp
+++ include/quicksort.hpp
@@ -6,11 +6,11 @@
 class Quicksort{
 	public:
 	Quicksort(int arr[], int arrSize);
+	bool IsSorted(int arr[], int leftBound, int rightBound);
 	
 	private:
 	int *left, *right, *pivot;
 	void Swap(int *a, int *b);
-	bool IsSorted(int arr[],int leftBound, int rightBound);
 	void Partition(int array[], int leftBound, int rightBound);
 	void AssignPointers(int arr[], int leftBound, int rightBound);
 	std::vector<int> GetIndexes(std::vector<int*>, int*);
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+
+//for random
 #include <cstdlib>
 #include <ctime>
+
+//for file input
+#include <fstream>
+
 #include "quicksort.hpp"
 
 int RandomNumber();
@@ -14,19 +20,29 @@ int main(){
 	for(int c = 0; c < num; c++){
 		std::cin >> arr[c];
 	}*/
-	int arr[10];
+	std::ofstream fout;
+	fout.open("input.in");
+	if(!fout){
+		std::cerr << "Failed to open the file" << std::endl;
+		return -1;
+	}
+	int arr[15];
 	srand(time(0));
-	for(int c = 0; c < 1000; c++){
-		for(int i = 0; i < 10; i++){
+	for(int c = 0; c < 5000; c++){
+		for(int i = 0; i < 15; i++){
 			arr[i] = RandomNumber();
 		}
 		std::cout << "\n";
-		Quicksort sort(arr, 10);
+		Quicksort sort(arr, 15);
 		for(int i : arr){
 			std::cout << i << "\t";
+			if(!sort.IsSorted(arr, 0, 14)){
+				fout << i << std::endl;
+			}
 		}
 		std::cout << "\n";
 	}
+	fout.close();
 	return 0;
 }
 
